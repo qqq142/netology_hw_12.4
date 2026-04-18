c4f267a5eb938a86cc822bd35cb061b2d5a4e6db
qqq142 <nekrasova.valeriia@mail.ru>
2026-04-18 21:09:01 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::ifstream file("in.txt");
+    if (!file.is_open()) {
+        std::cout << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+
+    int rows, cols;
+    file >> rows >> cols;
+
+    int** arr = new int*[rows];
+    for (int i = 0; i < rows; ++i) {
+        arr[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            file >> arr[i][j];
+        }
+    }
+    file.close();
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = cols - 1; j >= 0; --j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
